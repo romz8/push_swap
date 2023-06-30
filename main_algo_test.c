ebc4f476e494c410cbb864ed7fde856877c545a8
Romain Jobert <rjobert@student.42barcelona.com>
2023-06-30 18:09:52 +0200

    sorting algorithm nb int 5 max

--- main_algo_test.c
+++ main_algo_test.c
@@ -1,17 +1,19 @@
-#include "push_swap.h"
 
+#include "push_swap.h"
 int main(int argc, char *argv[])
 {
     t_list *a;
     t_list *b;
-
     b = NULL;
     a = NULL;
     create_stack(argc, argv, &a);
      ft_printf("Init stacks\n");
     print_stacks(&a, &b);
     //sort_3_ints(&a);
-    sort_5_ints(&a, &b);
+    sort_5max_ints(&a, &b);
     print_stacks(&a, &b);
     print_target(&b);
+    //print_target(&b);
+    //ft_printf("\n\n Now printing the stack a \n\n");
+    //print_target(&a);
 }
--- push_swap.h
+++ push_swap.h
@@ -9,21 +9,30 @@
 /*   Updated: 2023/06/15 17:32:52 by rjobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
 # include <stdlib.h>
 # include "./libft_printf/ft_printf.h"
 # include "./libft_printf/libft/libft.h"
-
 typedef struct s_stack {
     int     data;
     int     target;
+    int     rank;
+    int     move_count;
+    int     upper_half;
     struct s_stack *next;
     struct s_stack *prev; 
 }   t_list;
+
+/* struct explanation
+1 - data will be the number stored
+2 - target will be th enode on stack_a target by node on stack_b
+3 - rank is the position of the node in the stack (ascending) 
+4 - move_distance is how many moves a node needds to match its target
+5 - upper_half is to divide the sack in two part (to evaluate if 
+move_distance needs swap & rotate OR reverse rotate)
+6 - pointers to other nodes on the double linked list
+*/
 
 
 void    create_stack(int argc, char **argv, t_list **stack);
@@ -47,11 +56,26 @@ void    rra(t_list **a);
 void    rrb(t_list **b);
 void    rrr(t_list **a, t_list **b);
 void    sort_3_ints(t_list **stack);
-void    sort_5_ints(t_list **stack_a, t_list **stack_b);
+void    sort_5max_ints(t_list **stack_a, t_list **stack_b);
 int     stack_len(t_list **stack);
 int     find_stack_max(t_list **stack);
 int     find_stack_min(t_list **stack);
 int     find_target (t_list **stack, int node_val);
+void    move_distance(t_list **stack_a, t_list **stack_b);
+void    find_target_rank(t_list **stack, int target, int *data);
+void    rank_node_order(t_list **stack);
+void    evaluate_node(t_list **stack_a, t_list **stack_b);
+int    calculate_nb_move(t_list *head, t_list **stack_a, t_list **stack_b, int *data);
+void    key_nodes(t_list **stack_a, t_list **stack_b, t_list **move_node, t_list **target_node);
+void    rolling_engine(t_list **stack_a, t_list **stack_b);
+void    reverse_rotate_loop(t_list **stack_a, t_list **stack_b, t_list **target, t_list **node);
+void    rotate_loop(t_list **stack_a, t_list **stack_b, t_list **target, t_list **node);
+void    diagonale_rotate_up(t_list **stack_a, t_list **stack_b, t_list **target, t_list **node);
+void    diagonale_rotate_down(t_list **stack_a, t_list **stack_b, t_list **target, t_list **node);
+void    rearrange_stack(t_list **stack);
+void    reorder_down(t_list **stack, t_list **node);
+void    reorder_up(t_list **stack, t_list **node);
+
 
 void    print_target(t_list **stack);
 
